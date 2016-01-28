d30fba6004b3231bd6ca9162f275558da216f349
Alvin Wan <hi@alvinwan.com>
2016-01-28 00:42:14 -0800

    fixes #20 - set timezone for model

--- quupod/models.py
+++ quupod/models.py
@@ -27,7 +27,7 @@ class Base(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     updated_at = db.Column(ArrowType)
     updated_by = db.Column(db.Integer)
-    created_at = db.Column(ArrowType, default=arrow.utcnow())
+    created_at = db.Column(ArrowType, default=arrow.now('US/Pacific'))
     created_by = db.Column(db.Integer)
     is_active = db.Column(db.Boolean, default=True)
 
