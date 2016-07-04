556289ca04dd8a1ee31117f2560137dc933b15fc
Alvin Wan <hi@alvinwan.com>
2016-07-04 00:28:19 -0700

    cleaned up config

--- quupod/config.py
+++ quupod/config.py
@@ -1,30 +1,34 @@
-from urllib.parse import urlparse
+"""Temporary configuration file.
+
+TODO: Replace with a configuration file that Flask can digest.
+"""
+
 import os
 
 try:
     from configvars import ConfigVars
 except ImportError:
-    raise UserWarning('Configuration file not found. Rerun `make install` and \
-update the new configvars.py accordingly.')
+    from sampleconfigvars import ConfigVars
+    print(
+        'Configuration file not found. Rerun `make install` and update the new'
+        'configvars.py accordingly OR make sure your environment variables are'
+        'correct.')
 
 __all__ = ['config']
-
-# Extract information from environment.
-get = lambda v, default: os.environ.get(v, default)
 
 # Setup configuration dictionary, and check for environment variables.
 config = {}
 for attr in (
-    'username',
-    'password',
-    'host',
-    'port',
-    'database',
-    'secret_key',
-    'debug',
-    'googleclientID',
-    'allowed_netlocs',
-    'app_port',
-    'domain',
-    'timezone'):
-    config[attr] = get(attr, getattr(ConfigVars, attr))
+        'username',
+        'password',
+        'host',
+        'port',
+        'database',
+        'secret_key',
+        'debug',
+        'googleclientID',
+        'allowed_netlocs',
+        'app_port',
+        'domain',
+        'timezone'):
+    config[attr] = os.environ.get(attr.uppercase(), getattr(ConfigVars, attr))
