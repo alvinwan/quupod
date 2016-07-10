07ea05f335bb82d365d0f6c8fd87379bd50aa3bd
Alvin Wan <hi@alvinwan.com>
2016-07-09 19:07:08 -0700

    cleaned up public views

--- quupod/public/views.py
+++ quupod/public/views.py
@@ -1,17 +1,21 @@
-from flask import Blueprint, request, render_template, redirect, session, abort
-from flask import current_app
-from .forms import *
-from quupod.models import User, Inquiry, Queue
-from quupod.views import anonymous_required, render, url_for, current_url, current_user
-from quupod.views import login_manager
-from quupod.notifications import *
-from oauth2client import client, crypt
-import flask_login
-from apiclient.discovery import build
-import httplib2
+"""Handle all public pages."""
 
-# Google API service object for Google Plus
-service = build('plus', 'v1')
+from .logic import get_google_auth_flow
+from .logic import get_google_authorize_uri
+from .logic import get_google_person
+
+from flask import Blueprint
+from flask import request
+from flask import redirect
+from oauth2client import client
+from quupod.models import User
+from quupod.models import Queue
+from quupod.views import render
+from quupod.views import url_for
+from quupod.views import login_manager
+from werkzeug.local import LocalProxy
+
+import flask_login
 
 public = Blueprint('public', __name__, template_folder='templates')
 
@@ -19,53 +23,36 @@ public = Blueprint('public', __name__, template_folder='templates')
 # QUEUE #
 #########
 
+
 @public.route('/')
-def home():
-    """List of all 'unresolved' inquiries for the homepage"""
+def home() -> str:
+    """List of all 'unresolved' inquiries for the homepage."""
     return render('public/index.html', queues=Queue.query.all())
 
 ###################
 # SIGN IN/SIGN UP #
 ###################
 
-@public.route('/login', methods=['POST', 'GET'])
-def login(home=None, login=None):
-    """
-    Login using Google authentication
 
-    :param str home: URL for queue homepage
-    :param str login: URL for queue login page
+@public.route('/login', methods=['POST', 'GET'])
+def login(home: str=None, login: str=None) -> str:
+    """Login using Google authentication.
+
+    :param home: URL for queue homepage
+    :param login: URL for queue login page
     """
     try:
-        try:
-            flow = client.flow_from_clientsecrets(
-                'client_secrets.json',
-                scope='openid profile email',
-                redirect_uri=login or url_for('public.login', _external=True))
-        except:
-            flow = client.flow_from_clientsecrets(
-                '/storage/client_secrets.json',
-                scope='openid profile email',
-                redirect_uri=login or url_for('public.login', _external=True))
+        flow = get_google_auth_flow(login)
         if 'code' not in request.args:
-            auth_uri = flow.step1_get_authorize_url()
-            return redirect(auth_uri+'&prompt=select_account')
-        credentials = flow.step2_exchange(request.args.get('code'))
-        session['credentials'] = credentials.to_json()
-
-        http = credentials.authorize(httplib2.Http())
-        person = service.people().get(userId='me').execute(http=http)
-
+            return redirect(get_google_authorize_uri(flow))
+        person = get_google_person(flow)
         user = User.query.filter_by(google_id=person['id']).first()
         if not user:
             user = User(
                 name=person['displayName'],
                 email=person['emails'][0]['value'],
                 google_id=person['id'],
-                image_url=person['image']['url']
-            ).save()
-        else:
-            user.update(image_url=person['image']['url']).save()
+                image_url=person['image']['url']).save()
         flask_login.login_user(user)
         return redirect(home or url_for('public.home'))
     except client.FlowExchangeError:
@@ -77,30 +64,35 @@ def login(home=None, login=None):
 ######################
 
 @login_manager.user_loader
-def user_loader(id):
-    """Load user from a given integer id"""
+def user_loader(id: int) -> str:
+    """Load user from a given integer id."""
     return User.query.get(id)
 
+
 @login_manager.request_loader
-def request_loader(request):
-    """Loads user from Flask Request object"""
+def request_loader(request: LocalProxy) -> str:
+    """Load user from Flask Request object."""
     user = User.query.get(int(request.form.get('id') or 0))
     if not user:
         return
     user.is_authenticated = user.password == request.form['password']
     return user
 
-@public.route('/logout')
-def logout(home=None):
-    """
-    Logs out current session and redirects to home
 
-    :param str home: URL to redirect to after logout success
+@public.route('/logout')
+def logout(home: str=None) -> str:
+    """Log out current session and redirect to home.
+
+    :param home: URL to redirect to after logout success
     """
     flask_login.logout_user()
-    return redirect(request.args.get('redirect',
-        home or url_for('public.home')))
+    return redirect(
+        request.args.get(
+            'redirect',
+            home or url_for('public.home')))
+
 
 @login_manager.unauthorized_handler
 def unauthorized_handler():
+    """Redirect unauthorized users to the public queue home page."""
     return redirect(url_for('public.home'))
